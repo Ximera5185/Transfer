6901e62ba3dbea13541591d356ff3b3af3265030
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-06 09:59:37 +0300

    Добавьте файлы проекта.

---
+++ Transfer/Fighter.cs
@@ -0,0 +1,12 @@
+﻿namespace Transfer
+{
+    internal class Fighter
+    {
+        public Fighter(string name) 
+        {
+            Name = name;
+        }
+
+        public string Name{get; private set; }
+    }
+}
