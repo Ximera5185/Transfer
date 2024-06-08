e595a2893324acee89eadadf29cdb53573d6c333
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-08 06:48:21 +0300

    1

--- Transfer/Database.cs
+++ Transfer/Database.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Transfer
 {
@@ -11,48 +9,49 @@ namespace Transfer
         private List<Fighter> _fighters1 = new List<Fighter>();
         private List<Fighter> _fighters2 = new List<Fighter>();
 
-        public Database() { }
-
-        public void ShowFighters(int numberSquad) 
+        public Database()
         {
-            if (numberSquad == 1) 
-            {
-                foreach (Fighter fighter in _fighters1)
-                {
-                    Console.WriteLine(fighter.Name);
-                }
-            }
-            else if(numberSquad == 2)
-            {
-                foreach (Fighter fighter in _fighters2)
-                {
-                    Console.WriteLine(fighter.Name);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Такого отряда нет");
-            }
+            AddFighters();
         }
 
-        public void TransferFighters() 
+        public void TransferFighters()
         {
-            
+            string simbol = "б";
+
+            _fighters2.AddRange(_fighters1.Where(fighters => fighters.Name.StartsWith(simbol, StringComparison.OrdinalIgnoreCase)));
+
+            _fighters1.RemoveAll(fighter => fighter.Name.StartsWith(simbol, StringComparison.OrdinalIgnoreCase));
         }
 
-        private void AddFighters() 
+        public void ShowAllFighters()
         {
-            _fighters1.Add(new Fighter("Рагулин"));
-            _fighters1.Add(new Fighter("Мешков"));
-            _fighters1.Add(new Fighter("Попов"));
-            _fighters1.Add(new Fighter("Слонов"));
-            _fighters1.Add(new Fighter("Артемонов"));
+            ShowFighters(_fighters1);
 
-            _fighters2.Add(new Fighter("Багулин"));
-            _fighters2.Add(new Fighter("Бабуинов"));
-            _fighters2.Add(new Fighter("Брызгин"));
-            _fighters2.Add(new Fighter("Клопов"));
-            _fighters2.Add(new Fighter("Калинин"));
+            ShowFighters(_fighters2);
+        }
+        private void ShowFighters(List<Fighter> fighters)
+        {
+            foreach (Fighter fighter in fighters)
+            {
+                Console.WriteLine(fighter.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+        private void AddFighters()
+        {
+            _fighters1.Add(new Fighter("Багулин"));
+            _fighters1.Add(new Fighter("Бабуинов"));
+            _fighters1.Add(new Fighter("Брызгин"));
+            _fighters1.Add(new Fighter("Клопов"));
+            _fighters1.Add(new Fighter("Калинин"));
+
+            _fighters2.Add(new Fighter("Рагулин"));
+            _fighters2.Add(new Fighter("Мешков"));
+            _fighters2.Add(new Fighter("Попов"));
+            _fighters2.Add(new Fighter("Слонов"));
+            _fighters2.Add(new Fighter("Артемонов"));
         }
     }
 }
--- Transfer/Program.cs
+++ Transfer/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Transfer
 {
@@ -10,6 +6,18 @@ namespace Transfer
     {
         static void Main(string [] args)
         {
+            Database database = new Database();
+
+            database.ShowAllFighters();
+            
+
+            database.TransferFighters();
+
+
+            database.ShowAllFighters();
+           
+
+            Console.ReadKey();
         }
     }
 }
